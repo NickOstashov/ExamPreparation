855853be5e04bad9f26d73b8ad0d5f09941aaa19
NickOstashov <111501728+NickOstashov@users.noreply.github.com>
2025-03-22 13:12:08 +0500

    upd

--- task_24/sliding_window.py
+++ task_24/sliding_window.py
@@ -3,28 +3,24 @@
 нужно найти попоследовательность из 3-х чисел с максимальной суммой
 """
 import random
+import sys
 
 def solution():
-    # Генерация случайной длины массива от 1000 до 100_000
-    length = random.randint(1000, 100_000)
+    arr = [2, 1, -10, 11, -1, 3, 2]
 
-    # Генерация массива с произвольными числами от -5 до 5
-    random_array = [random.randint(-5, 5) for _ in range(length)]
-
-    arr = [2, 1, 4, 5, -10, 1]
-
-    max_sum = -1
+    max_sum = -sys.maxsize
     for i in range(len(arr) - 2):
         l_sum = sum(arr[i: i + 3])
 
         if l_sum > max_sum:
             max_sum = l_sum
 
-    return max_sum
+    print(max_sum)
 
 
 def main():
-    print(solution())
+    solution()
+
 
 if __name__ == "__main__":
     main()
